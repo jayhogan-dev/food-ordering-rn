27ab0560c4a6ea1cae6491e4e61ef31607f12af0
Jay Hogan <jayhogan.dev@gmail.com>
2024-06-18 12:00:27 -0400

    adding a product api created

--- src/api/products/index.ts
+++ src/api/products/index.ts
@@ -1,5 +1,5 @@
 import { supabase } from '@/src/lib/supabase';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 export const useProductList = () => {
   return useQuery({
@@ -31,6 +31,35 @@ export const useProduct = (id: number) => {
       }
 
       return data;
+    },
+  });
+};
+
+export const useInsertProduct = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    async mutationFn(data: any) {
+      const { data: newProduct, error } = await supabase
+        .from('products')
+        .insert({
+          name: data.name,
+          image: data.image,
+          price: data.price,
+        })
+        .single();
+
+      if (error) {
+        throw new Error(error.message);
+      }
+
+      return newProduct;
+    },
+    // by doing this, we can refresh the products on the previous screen "menu"
+    async onSuccess() {
+      await queryClient.invalidateQueries({
+        queryKey: ['products'],
+      });
     },
   });
 };
--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -4,7 +4,8 @@ import Colors from '@/src/constants/Colors';
 import { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Image, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import { Stack, useLocalSearchParams } from 'expo-router';
+import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
+import { useInsertProduct } from '@/src/api/products';
 
 const CreateProductScreen = () => {
   const [name, setName] = useState('');
@@ -14,6 +15,10 @@ const CreateProductScreen = () => {
 
   const { id } = useLocalSearchParams();
   const isUpdating = !!id;
+
+  const { mutate: insertProduct } = useInsertProduct();
+
+  const router = useRouter();
 
   const resetFields = () => {
     setName('');
@@ -53,10 +58,16 @@ const CreateProductScreen = () => {
       return;
     }
 
-    console.warn('Create product');
-
-    // after values are submitted
-    resetFields();
+    insertProduct(
+      { name, price: parseFloat(price), image },
+      {
+        onSuccess: () => {
+          // after values are submitted
+          resetFields();
+          router.back();
+        },
+      },
+    );
   };
 
   const onUpdate = () => {
